6dd079c3f5b7a79c808922841deedb649784f59a
Yusuf <yusufsedef0@gmail.com>
2024-09-23 22:55:12 +0300

    v3
    added function

--- dict.py
+++ dict.py
@@ -8,8 +8,58 @@ db = mysql.connector.connect(
     password = "Yusuf2005.",
     database = "eng-tr"
 )
-
 cursor = db.cursor()
+
+
+def results():
+    cursor.execute("SELECT id,tr,eng FROM dict")
+    res = cursor.fetchall()
+    print(res)
+
+def correction():
+    english = input("ingilizce kelime: ")
+    türkçe = input("türkçe karşılığı: ")
+    id = input("id: ")
+    sql = cursor.execute("UPDATE dict SET eng=%s, tr=%s WHERE id=%s",(english,türkçe,id))
+    db.commit()
+
+
+def addWord():
+    english = input("ingilizce kelime: ")
+    cursor.execute("SELECT eng FROM dict")
+    control = cursor.fetchall()
+    for i in control:
+        if i[0] == english:
+            print("Bu kelime zaten var!")
+            break
+        else:
+            türkçe = input("türkçe karşılığı: ")
+            sql = cursor.execute("INSERT INTO dict VALUES(%s,%s,%s)",(None,english,türkçe))
+            db.commit()
+            break
+
+def question():
+    whl = True
+    while whl == True:
+        os.system("cls")
+        cursor.execute("SELECT COUNT(*) FROM dict")
+        result = cursor.fetchall()
+        word_id = random.randint(1,result[0][0])
+
+        cursor.execute("SELECT eng FROM dict WHERE id=%s",([word_id]))
+        eng = cursor.fetchone()
+
+        cursor.execute("SELECT tr FROM dict WHERE id=%s",([word_id]))  
+        tr= cursor.fetchone()    
+
+        print(tr[0])
+        translate = input("İngilizcesi: ").lower()
+        if translate == eng[0]:
+            print("Doğru")
+        else:
+            print("Yanlış")
+            whl = False
+
 
 cont = input("giriş - çıkış: ").lower()
 while cont == "giriş":
@@ -18,62 +68,25 @@ while cont == "giriş":
     os.system("cls")
 
     if menü == "1": #kelime görme
-        cursor.execute("SELECT id,tr,eng FROM dict")
-        res = cursor.fetchall()
-        print(res)
+        results()
 
     elif menü == "2": # kelime ekleme
         os.system("cls")
-
-        english = input("ingilizce kelime: ")
-        cursor.execute("SELECT eng FROM dict")
-        control = cursor.fetchall()
-        for i in control:
-            if i[0] == english:
-                print("Bu kelime zaten var!")
-                break
-            else:
-                türkçe = input("türkçe karşılığı: ")
-                sql = cursor.execute("INSERT INTO dict VALUES(%s,%s,%s)",(None,english,türkçe))
-                db.commit()
-                break
+        addWord()
 
 
-    # elif menü == "3":
+    # elif menü == "5":
     #     delete = input("silmek istenilen id= ")
-    #     sql = cursor.execute("DELETE FROM dict WHERE id=(%s)",(delete))
+    #     sql = cursor.execute("DELETE FROM dict WHERE id=(%s)",([delete]))
     #     db.commit()
 
 
     elif menü == "3": #kelime düzelt
         os.system("cls")
-        english = input("ingilizce kelime: ")
-        türkçe = input("türkçe karşılığı: ")
-        id = input("id: ")
-        sql = cursor.execute("UPDATE dict SET eng=%s, tr=%s WHERE id=%s",(english,türkçe,id))
-        db.commit()
+        correction()
 
     elif menü == "4": # kelime sorma
         os.system("cls")
-        whl = True
-        while whl == True:
-            os.system("cls")
-            cursor.execute("SELECT COUNT(*) FROM dict")
-            result = cursor.fetchall()
-            word_id = random.randint(1,result[0][0])
-
-            cursor.execute("SELECT eng FROM dict WHERE id=%s",([word_id]))
-            eng = cursor.fetchone()
-
-            cursor.execute("SELECT tr FROM dict WHERE id=%s",([word_id]))  
-            tr= cursor.fetchone()    
-
-            print(tr[0])
-            translate = input("İngilizcesi: ").lower()
-            if translate == eng[0]:
-                print("Doğru")
-            else:
-                print("Yanlış")
-                whl = False
+        question()
 
     cont = input("giriş - çıkış: ").lower()
